24450e69ea2fc473fe64869b03475a7c7be7deab
Wolford Li <wolfordli@outlook.com>
2025-02-10 14:00:54 +0800

    Create install.sh

---
+++ nat_dns/install.sh
@@ -0,0 +1,3 @@
+curl -L -o /etc/dnsmasq.conf https://raw.githubusercontent.com/liworlford/Luna_DNS/refs/heads/main/dnsmasq.conf
+wget -O nf https://github.com/sjlleo/netflix-verify/releases/download/v3.1.0/nf_linux_amd64 && chmod +x nf
+curl -L -o main.py https://raw.githubusercontent.com/liworlford/supports_store/refs/heads/main/nat_dns/main.py
